9a2226d424960faf9355a8077df8d6268d7f666d
Ayush <ayushbisht750083@gmail.com>
2024-09-13 10:35:37 +0530

    made the full checking the error and ui

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,8 +3,9 @@ import "./Header.css"
 import { Box } from '../Box/Box'
 
 
-export const Header = () => {
+export const Header = ({ grouping, setGrouping, ordering, setOrdering }) => {
   return (
-    <div className='box' ><Box/></div>
+    <div className='box'  ><Box grouping={grouping} setGrouping={setGrouping} ordering={ordering} setOrdering={setOrdering} /></div>
   )
 }
+
